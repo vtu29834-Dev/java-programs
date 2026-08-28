0a5f5eab9580f0acffe1cae40b7ad2d7cf6a60d0
ch.purna reddy <vtu29834@veltech.edu.in>
2026-08-28 09:45:47 +0530

    Create week3-vtu29834:-day of the year.java

---
+++ week3-vtu29834:-day of the year.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int dayOfYear(String date) {
+
+        String[] parts = date.split("-");
+
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+
+        int[] days = {
+            31, 28, 31, 30, 31, 30,
+            31, 31, 30, 31, 30, 31
+        };
+
+        // Leap year
+        if ((year % 400 == 0) ||
+            (year % 4 == 0 && year % 100 != 0)) {
+            days[1] = 29;
+        }
+
+        int result = 0;
+
+        // Add days of previous months
+        for (int i = 0; i < month - 1; i++) {
+            result += days[i];
+        }
+
+        // Add current day
+        result += day;
+
+        return result;
+    }
+}
