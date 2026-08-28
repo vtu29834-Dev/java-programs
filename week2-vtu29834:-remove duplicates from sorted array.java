57d925ca625b782e859c9b06bcc7b643d1d10ab1
ch.purna reddy <vtu29834@veltech.edu.in>
2026-08-28 09:29:47 +0530

    Create week2-vtu29834:-remove duplicates from sorted array.java

---
+++ week2-vtu29834:-remove duplicates from sorted array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
+output:-
+  2
