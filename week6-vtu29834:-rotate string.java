30c63d67a724b3a8133609a1e665632276a6eb33
ch.purna reddy <vtu29834@veltech.edu.in>
2026-08-29 10:19:31 +0530

    Create week6-vtu29834:-rotate string.java

---
+++ week6-vtu29834:-rotate string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String combined = s + s;
+
+        return combined.contains(goal);
+    }
+}
+output:-
+  true
