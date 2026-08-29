f5f2c83dae4f3aefec7a7c65f495bd0295de3a01
ch.purna reddy <vtu29834@veltech.edu.in>
2026-08-29 10:16:31 +0530

    Create week6-vtu29834:-repeated substring.java

---
+++ week6-vtu29834:-repeated substring.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
+output:-
+  Input:  "aba"
+Output: false
