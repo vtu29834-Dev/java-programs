2fccfea597d4207d5b0bded746d2acffbed89074
ch.purna reddy <vtu29834@veltech.edu.in>
2026-09-11 09:26:10 +0530

    Create week7-vtu29834:-design hashset.java

---
+++ week7-vtu29834:-design hashset.java
@@ -0,0 +1,25 @@
+class MyHashSet {
+
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+ouput:-
+  true
+false
+true
+false
