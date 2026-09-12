84073ebbc219a8746ea29c6dcdd61bd8229f9cd2
ch.purna reddy <vtu29834@veltech.edu.in>
2026-09-12 10:01:05 +0530

    Create week8-vtu29834:-method overriding.java

---
+++ week8-vtu29834:-method overriding.java
@@ -0,0 +1,45 @@
+class Sports {
+    String getName() {
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers() {
+        System.out.println(
+            "Each team has n players in " + getName()
+        );
+    }
+}
+
+class Soccer extends Sports {
+
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println(
+            "Each team has 11 players in " + getName()
+        );
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Sports sports = new Sports();
+        Soccer soccer = new Soccer();
+
+        System.out.println(sports.getName());
+        sports.getNumberOfTeamMembers();
+
+        System.out.println(soccer.getName());
+        soccer.getNumberOfTeamMembers();
+    }
+}
+output:-
+  Generic Sports
+Each team has n players in Generic Sports
+Soccer Class
+Each team has 11 players in Soccer Class
