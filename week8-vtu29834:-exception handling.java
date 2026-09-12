b299c35ef4dd79c422a763a44e305b00ee7f3355
ch.purna reddy <vtu29834@veltech.edu.in>
2026-09-12 09:11:20 +0530

    Create week8-vtu29834:-exception handling.java

---
+++ week8-vtu29834:-exception handling.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a / b);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+
+        sc.close();
+    }
+}
+output:-
+3
